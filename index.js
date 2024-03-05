14ad3b9a77b7b35336c27105321fa16f52aefb36
Kehinde4885 <Kehindemalagu@gmail.com>
2024-03-05 01:54:08 +0100

    Landing Page Completed. Project page ongoing Refactored Javasxript Functions

--- index.js
+++ index.js
@@ -6,68 +6,56 @@ async function dataFetching() {
   return data;
 }
 
-async function renderArticles() {
+async function renderPage() {
   let data = await dataFetching();
 
+
+  renderArticles(data.articles);
+
+  renderProjects(data.projects)
+
+  renderProjectsList(data.projects)
+}
+
+
+
+function renderArticles(articlesarr) {
   let articles = document.getElementById("articles");
 
-  let innerhtml = ""
+  let innerhtml = "";
 
-  data.projects.forEach((project) => {
-    
+  articlesarr.forEach((article) => {
     innerhtml += `<article class="min-w-[310px] space-y-7">
         <div class="flex gap-x-2">
           <span class="h-[22px] w-[22px] shrink-0 rounded-full bg-yellow">
           </span>
           <div class="space-y-3">
-            <p class="text-base">
-              ${project.desc}
+            <p class="text-base md:text-lg lg:text-xl">
+              ${article.desc}
             </p>
-            <button class="text-sm">see ${project.category} projects →</button>
+            <button class="text-sm">see ${article.category} projects →</button>
           </div>
         </div>
         
-        <div class="bg-ivory">
+        <div class="bg-black">
           <img
             class="mx-auto my-auto h-4/5 w-4/5"
-            src=${project.image}
+            src=${article.image}
             alt=""
           />
         </div>
         </article>`;
 
-    articles.innerHTML= innerhtml;
+    articles.innerHTML = innerhtml;
   });
 }
 
-renderArticles();
 
-let projectsarr = [
-  {
-    name: "Apartment in Holmengrenda",
-    images: ["./images/Holmeng1.jpg", "./images/Holmeng2.jpg"],
-    location: "Oslo",
-  },
-  {
-    name: "Cabin in the archipelago",
-    images: ["./images/archi1.jpg"],
-    location: "Agder",
-  },
-  {
-    name: "Detached House in Lillesand",
-    images: ["./images/lille1.jpg", "./images/lille2.jpg"],
-    location: "Nottoroy",
-  },
-];
 
-function renderProjects() {
-  let projects = document.getElementById("projects");
+function renderProjects(projects) {
+  let projectswrapper = document.getElementById("projects");
 
-  function renderImages(stringarr) {
-    console.log("");
-  }
-
-  projectsarr.forEach((project, index) => {
+  projects.forEach((project, index) => {
     //console.log(project.images)
     let article = document.createElement("article");
 
@@ -88,34 +76,51 @@ function renderProjects() {
 
     //add Title
     let title = document.createElement("p");
-    title.classList.add("title", "mt-2");
+    title.classList.add("title", "mt-2", "md:text-xl");
     title.innerText = `${project.name}`;
 
     article.append(title);
 
     //add complete element to DOM
-    projects.append(article);
+    projectswrapper.append(article);
   });
 }
 
-renderProjects();
 
 //Render Projects List
 
-function renderProjectsList() {
+function renderProjectsList(projects) {
   let list = document.getElementById("list");
 
   let innerhtml = "";
 
-  projectsarr.forEach((project) => {
+  projects.forEach((project) => {
+    innerhtml += `<li class="py-4 border-t border-khaki  grid grid-cols-3 min-h-[60px] xl:min-h-[92px] items-center  gap-x-9">
     
-    innerhtml += `<li class="border-t border-khaki  flex h-[60px] items-center justify-between">
-    <p>${project.name}</p>
-    <p class="text-sm">${project.location}</p>
+    <p class="md:text-lg lg:text-xl">${project.name}</p>
+
+    <ul class="flex-row flex gap-x-4 flex-wrap max-w-[calc(4*8ch)]">
+
+    ${categories(project.categories)}
+  
+    </ul>
+
+    <p class="justify-self-end text-sm">${project.location}</p>
   </li>`;
   });
 
-  list.innerHTML = innerhtml
+  function categories(arr) {
+    let categories = "";
+
+    arr.forEach((cat) => {
+      categories += `<li class="capitalize">${cat}</li>`;
+    });
+
+    return categories;
+  }
+
+  list.innerHTML = innerhtml;
 }
 
-renderProjectsList();
+
+renderPage();
